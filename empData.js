a181d439113c98395703b0f3d09cf152e5b85508
Dnyaneshwar Argade <dnyaneshwarargade8@gmail.com>
2025-09-13 15:34:29 +0530

    6 :-  Create & Test API in Node JS with POSTMAN

---
+++ empData.js
@@ -0,0 +1,8 @@
+const empData = [
+    { name  : "Dnyaneshwar", age : 21},
+    { name : "Mauli", age : 21},
+    { name : "Sanket", age : 21},
+    { name : "Sankalp", age : 21},
+    { name : "Rohit", age : 21},
+]
+module.exports = empData;
